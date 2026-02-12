b289dd69dfa5c83ff19545bee390da69bd153604
Mesbahul Islam <mesbahultahiat@gmail.com>
2026-02-12 02:05:24 +0200

    Add initial project structure with Docker support and example services

---
+++ hello_rx/hello_rx.py
@@ -0,0 +1,13 @@
+import socket
+import time
+
+
+def main() -> None:
+    hostname = socket.gethostname()
+    while True:
+        print(f"Hello RX from {hostname}!", flush=True)
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
